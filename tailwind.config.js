85df621162ca3fd89ce822e22b4c78b38327537f
Adil9086 <154617069+Adil9086@users.noreply.github.com>
2024-11-26 18:31:24 +0500

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Adjust based on your project
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "rise-section": "riseSection 1.5s ease-out",
+        rise: "riseFromDown 1.5s ease-out", // Animation for text and button
+      },
+      keyframes: {
+        riseSection: {
+          "0%": { transform: "translateY(100%)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+        riseFromDown: {
+          "0%": { transform: "translateY(100%)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
